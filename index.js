82a179cbfffe14d9fe1512b7989df26b7fbc2922
Md Fahim Faisal <mdfahimfaisal000@gmail.com>
2022-11-08 01:59:54 +0600

    db setup

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
@@ -13,6 +14,20 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Hello from Hay Day Server');
 });
+
+
+
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.tzinyke.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+    const servicesCollection = client.db('hayday-db').collection('services');
+}
+
+run().catch(err => console.error(err));
+
 
 app.listen(port, () => {
     console.log('Hay Day server is running on port :', port);
