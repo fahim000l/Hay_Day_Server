f8619c139e6682e46916ea6ef38c98492c95c77d
Md Fahim Faisal <mdfahimfaisal000@gmail.com>
2022-11-08 02:16:45 +0600

    services get request

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv');
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
@@ -18,12 +18,26 @@ app.get('/', (req, res) => {
 
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.tzinyke.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tzinyke.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
 
     const servicesCollection = client.db('hayday-db').collection('services');
+
+    app.get('/services', async (req, res) => {
+        const query = {};
+        const cursor = servicesCollection.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
+    });
+
+    app.get('/services/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const service = await servicesCollection.findOne(query);
+        res.send(service);
+    });
 }
 
 run().catch(err => console.error(err));
